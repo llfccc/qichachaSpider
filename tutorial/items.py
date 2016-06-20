300136236a85655029a582d1971d6d97c20c3170
llfccc <llfccc@live.cn>
2016-06-20 16:28:46 +0800

    inti

---
+++ tutorial/items.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class DmozItem(scrapy.Item):
+    company_name = scrapy.Field()
+    artificial_person = scrapy.Field()
+    year = scrapy.Field()
+    href = scrapy.Field()
+    registration_number = scrapy.Field()
+
