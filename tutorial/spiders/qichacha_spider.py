58b82ebf04f2af1741d10cc432f2d47586193557
llfccc <llfccc@live.cn>
2016-06-27 17:33:57 +0800

    modify

--- tutorial/spiders/qichacha_spider.py
+++ tutorial/spiders/qichacha_spider.py
@@ -35,31 +35,32 @@ class MySpider(scrapy.Spider):
     def parse(self, response):
         href = response.xpath('//*[@id="searchlist"]/ul/a[1]/@href').extract()[0].split('.')[0][9:]
         time.sleep(3)
-        cookies = {'gr_user_id': '753e88a4-467d-47ab-8c45-2ef617834e0a', 'PHPSESSID': 'nfe4n08cudpfq4kjr1kdad4o81',
-                   'SERVERID': 'b7e4e7feacd29b9704e39cfdfe62aefc | 1466661976 | 1466661835',
-                   'gr_session_id_9c1eb7420511f8b2': '72cf2d1f-97a4-46d8-a69f-89758e2a5f26'}
-        href1 = 'http://www.qichacha.com/company_getinfos?unique=' + href + '&companyname=%E6%9D%AD%E5%B7%9E%E6%B5%99%E5%9C%B0%E9%9D%9E%E9%87%91%E5%B1%9E%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8&tab=base'
-        href2 = 'http://www.qichacha.com/company_getinfos?unique=' + href + '&companyname=%E6%9D%AD%E5%B7%9E%E6%B5%99%E5%9C%B0%E9%9D%9E%E9%87%91%E5%B1%9E%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8&tab=report'
-
+        cookies = {'gr_user_id': '753e88a4-467d-47ab-8c45-2ef617834e0a',
+                   'PHPSESSID': '1c0c8jh2i8ktljhqn3upi01mv4',
+                   'SERVERID': 'b7e4e7feacd29b9704e39cfdfe62aefc|1466989946|1466987928',
+                   'gr_session_id_9c1eb7420511f8b2': '0b1de50e-09f0-40bb-ae92-dca93f8e83e9'}
+        href_base = 'http://www.qichacha.com/company_getinfos?unique=' + href + '&companyname=%E6%9D%AD%E5%B7%9E%E6%B5%99%E5%9C%B0%E9%9D%9E%E9%87%91%E5%B1%9E%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8&tab=base'
+        href_report = 'http://www.qichacha.com/company_getinfos?unique=' + href + '&companyname=%E6%9D%AD%E5%B7%9E%E6%B5%99%E5%9C%B0%E9%9D%9E%E9%87%91%E5%B1%9E%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8&tab=report'
         item = DmozItem()
-
         if href:
-            yield Request(href1, cookies=cookies, callback=self.parse_item, meta={'item': item,''})
-            yield Request(href2, cookies=cookies, callback=self.parse_item, meta={'item': item})
-        yield item
+            yield Request(href_base, cookies=cookies, callback=self.parse_item, meta={'item': item, 'category': 'base'})
+            time.sleep(3)
+            yield Request(href_report, cookies=cookies, callback=self.parse_item, meta={'item': item, 'category': 'report'})
+        print (item)
 
     def parse_item(self, response):
-        item = DmozItem()
-        # item['company_name'] = response.xpath('//*[@id="company-top"]/div/div[1]/span[2]/span[1]/text()').extract()[0]
-        # item['registration_number'] = response.xpath('//section[1]/div[2]/ul/li[1]/text()').extract()[0]
-        output = response.body
-
         item = response.meta['item']
-        item['company_name']='a'
-        f = open('f.txt', 'a')
-        f.write(output)
-        f.close()
-        # item['artificial_person'] = response.xpath('//*[@id="searchlist"]/ul/a[1]/span[2]/small[1]/text()[2]').extract()[0]
-        # item['year'] = response.xpath('//*[@id="searchlist"]/ul/a[1]/span[2]/small[1]/text()[3]').extract()[0]
+        category = response.meta['category']
+        if category == 'base':
+            item['registration_number'] = response.xpath('//section[1]/div[2]/ul/li[1]/text()').extract()[0]
+            yield item
+        if category == 'report':
+            item['year'] = response.xpath('//section[1]/div[1]/ul[1]/li[1]/a/span/text()').extract()[0]
+
+
+
         #
-        yield item
+        # output = response.body
+        # f = open('f.txt', 'a')
+        # f.write(output)
+        # f.close()
